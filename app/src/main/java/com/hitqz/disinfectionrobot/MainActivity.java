ec04234478864490b52d8bd11e3c3d457a8d1001
iponkan <iponkan@gmail.com>
2022-06-28 16:53:16 +0800

    沉浸式状态栏

--- app/src/main/java/com/hitqz/disinfectionrobot/MainActivity.java
+++ app/src/main/java/com/hitqz/disinfectionrobot/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gyf.immersionbar.ImmersionBar;
 import com.hitqz.disinfectionrobot.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ImmersionBar.with(this)
+                .statusBarColor(R.color.transparent).statusBarDarkFont(true).init();
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
     }
