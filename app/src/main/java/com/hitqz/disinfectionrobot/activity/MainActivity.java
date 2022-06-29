70c88c45811bc6ee858a99451825b64e4723e3a0
iponkan <iponkan@gmail.com>
2022-06-29 15:52:45 +0800

    首页框架

--- app/src/main/java/com/hitqz/disinfectionrobot/activity/MainActivity.java
+++ app/src/main/java/com/hitqz/disinfectionrobot/activity/MainActivity.java
@@ -2,14 +2,18 @@ package com.hitqz.disinfectionrobot.activity;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.FragmentTransaction;
+
 import com.gyf.immersionbar.ImmersionBar;
 import com.hitqz.disinfectionrobot.R;
 import com.hitqz.disinfectionrobot.databinding.ActivityMainBinding;
+import com.hitqz.disinfectionrobot.fragment.MainFragment;
 import com.hitqz.disinfectionrobot.i.IGo;
 
 public class MainActivity extends BaseActivity implements IGo {
 
     ActivityMainBinding mBinding;
+    private MainFragment mMainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +22,24 @@ public class MainActivity extends BaseActivity implements IGo {
                 .statusBarColor(R.color.transparent).statusBarDarkFont(true).init();
         mBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        go2Main();
     }
 
     @Override
     public void go2Main() {
-//        hideOther();
-//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        if (mLoginFragment == null) {
-//            mLoginFragment = LoginFragment.newInstance();
-//            fragmentTransaction.add(R.id.vp_overlap, mLoginFragment);
-//        } else {
-//            fragmentTransaction.show(mLoginFragment);
-//        }
-//
-//        fragmentTransaction.commitAllowingStateLoss();
+        hideOther();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        if (mMainFragment == null) {
+            mMainFragment = MainFragment.newInstance();
+            fragmentTransaction.add(R.id.vp_content, mMainFragment);
+        } else {
+            fragmentTransaction.show(mMainFragment);
+        }
+
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+    private void hideOther() {
+
     }
 }
