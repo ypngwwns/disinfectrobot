8e8fe232ce44d9a55d5b822bee091859da0ada36
iponkan <iponkan@gmail.com>
2022-07-15 16:10:56 +0800

    切换地图

--- app/src/main/java/com/hitqz/disinfectionrobot/activity/ToggleMapActivity.java
+++ app/src/main/java/com/hitqz/disinfectionrobot/activity/ToggleMapActivity.java
@@ -6,12 +6,32 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 
+import com.hitqz.disinfectionrobot.data.MapData;
 import com.hitqz.disinfectionrobot.databinding.ActivityToggleMapBinding;
+import com.hitqz.disinfectionrobot.dialog.MapListDialog;
+import com.hitqz.disinfectionrobot.dialog.ToggleMapAlertDialog;
+import com.hitqz.disinfectionrobot.util.PathUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 @SuppressLint("CheckResult")
 public class ToggleMapActivity extends BaseActivity {
 
     ActivityToggleMapBinding mBinding;
+    private List<String> mList;
+    private MapData mMapData;
+
+    private void initMap(String mapCode) {
+        mMapData = new MapData(PathUtil.getMapPGMFile(getApplicationContext(), mapCode),
+                PathUtil.getMapYmlFile(getApplicationContext(), mapCode));
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +42,104 @@ public class ToggleMapActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        ToggleMapAlertDialog dialog = new ToggleMapAlertDialog();
+        dialog.setOnClickListener(new ToggleMapAlertDialog.OnClickListener() {
+            @Override
+            public void onConfirm() {
+
+            }
+
+            @Override
+            public void onCancel() {
+                finish();
+            }
+        });
+        dialog.show(getSupportFragmentManager(), ToggleMapAlertDialog.TAG);
+
+        showDialog();
+        Observable
+                .fromCallable(() -> {
+                    initMap("map0622");
+                    return 0;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        dismissDialog();
+                        mBinding.tvMapCode.setText("map0622");
+                        mBinding.navigationView.setBitmap(mMapData.bitmap);
+                        mBinding.navigationView.setResolutionAndOrigin(mMapData.resolution, mMapData.originX, mMapData.originY);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        dismissDialog();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+        mList = new ArrayList<>();
+        mList.add("地图1");
+        mList.add("地图2");
+        mList.add("地图3");
+        mList.add("地图4");
+
+        mBinding.btnMapList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MapListDialog dialog = new MapListDialog();
+                dialog.setList(mList);
+                dialog.setOnClickListener(new MapListDialog.OnClickListener() {
+                    @Override
+                    public void onConfirm(String text) {
+                        showDialog();
+                        Observable
+                                .fromCallable(() -> {
+                                    initMap("map0622");
+                                    return 0;
+                                })
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Observer<Integer>() {
+                                    @Override
+                                    public void onSubscribe(Disposable d) {
+
+                                    }
+
+                                    @Override
+                                    public void onNext(Integer integer) {
+                                        dismissDialog();
+                                        mBinding.tvMapCode.setText(text);
+                                        mBinding.navigationView.setBitmap(mMapData.bitmap);
+                                        mBinding.navigationView.setResolutionAndOrigin(mMapData.resolution, mMapData.originX, mMapData.originY);
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+                                        dismissDialog();
+                                    }
+
+                                    @Override
+                                    public void onComplete() {
+
+                                    }
+                                });
+                    }
+                });
+                dialog.show(getSupportFragmentManager(), dialog.getTag());
             }
         });
     }
---
+++ app/src/main/java/com/hitqz/disinfectionrobot/dialog/ToggleMapAlertDialog.java
@@ -0,0 +1,92 @@
+package com.hitqz.disinfectionrobot.dialog;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.hitqz.disinfectionrobot.R;
+
+@SuppressLint("CheckResult")
+public class ToggleMapAlertDialog extends DialogFragment {
+
+    public static final String TAG = ToggleMapAlertDialog.class.getSimpleName();
+
+    private OnClickListener mOnClickListener;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.Theme_AppCompat_Dialog_Alert);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("注意")
+                .setMessage("必须要在充电点切换地图，否则机器人会丢失位置")
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+
+                    }
+                })
+                .setPositiveButton("确定", null)
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        if (mOnClickListener != null) {
+                            mOnClickListener.onCancel();
+                        }
+
+                        dismiss();
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialog) {
+                //重写“确定”，截取监听
+                alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (checkInValid()) return;
+
+                        if (mOnClickListener != null) {
+                            mOnClickListener.onConfirm();
+                        }
+
+                        dismiss();
+                    }
+                });
+            }
+        });
+
+        // Create the AlertDialog object and return it
+        return alertDialog;
+    }
+
+    private boolean checkInValid() {
+        return false;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.mOnClickListener = onClickListener;
+    }
+
+    public interface OnClickListener {
+        void onConfirm();
+
+        void onCancel();
+    }
+}
